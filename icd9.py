4ed0e4051352482b85a4fece5816a1cb04633504
Hidhe Garcia <hidhe.m.garcia@gmail.com>
2014-08-07 17:12:37 -0700

    successfully read file and built dictionary

--- icd9.py
+++ icd9.py
@@ -1,1 +1,15 @@
-print "hello"
+import os
+path = os.path.join("/home","hidhe","Documents","git","version 39","short_dx.txt")
+infile = open(path, 'r')
+d = {}
+
+for x in infile:
+    if len(x) < 5:
+        continue
+    code = x[0:5]
+    while code[-1] == " ":
+        code = code[:-1]
+    k = code
+    v = x[5:]
+    d[k] = v.strip()
+print d
